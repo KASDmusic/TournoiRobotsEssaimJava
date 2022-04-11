6c82a5a0ba0c382e896b5bec0b7218925e87c8ff
KASDmusic <83025562+KASDmusic@users.noreply.github.com>
2022-04-12 00:14:01 +0200

    deleting old files

---
+++ src/Controleur.java
@@ -0,0 +1,46 @@
+package src;
+
+import src.model.Terrain;
+import src.view.MainFrame;
+
+public class Controleur 
+{	
+	private Terrain terrain;
+	private MainFrame mainFrame;
+	
+	public Controleur()
+	{
+		this.terrain = new Terrain();
+		this.mainFrame = new MainFrame(this.terrain);
+		
+		while(this.terrain.getBalise().getToursRestants() > 0 && Terrain.nbTour < Terrain.NB_TOURS_LIMITE)
+		{
+			Terrain.nbTour++;
+			
+			for(int i=0 ; i<Terrain.NB_RBT ; i++)
+			{
+				//Condition creuser balise et detection Balise
+				this.terrain.detectionCreuserBalise();
+				
+				if(this.terrain.getBalise().getToursRestants() <= 0)
+					break;
+				
+				//Condition Communication Robot
+				this.terrain.communicationRobot();
+				
+				this.terrain.getEnsPiloteRobot().get(i).pilote();
+			}
+			
+			this.mainFrame.repaint();
+			
+			try { Thread.sleep(100); }
+			catch(Exception e) { System.out.println(e); }
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		new Controleur();
+	}
+
+}
