a9632a10804c9cec715711f0e8913de4f9b5ae82
KASDmusic <83025562+KASDmusic@users.noreply.github.com>
2022-04-12 00:12:27 +0200

    Code organization

---
+++ src/model/Balise.java
@@ -0,0 +1,21 @@
+package src.model;
+
+public class Balise 
+{
+	private int x;
+	private int y;
+	private int toursRestants;
+	
+	public Balise(int x, int y, int toursRestants)
+	{
+		this.x = x;
+		this.y = y;
+		this.toursRestants = toursRestants;
+	}
+
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public int getToursRestants() { return toursRestants; }
+	
+	public void creuser() { this.toursRestants--; }
+}
---
+++ src/model/PiloteRobot.java
@@ -0,0 +1,28 @@
+package src.model;
+
+public class PiloteRobot 
+{	
+	private Robot robot;
+	
+	public PiloteRobot(Robot robot)
+	{
+		this.robot = robot;
+	}
+	
+	//A Completer
+	public void pilote()
+	{
+		
+		char[] tab = new char[] {'N', 'S', 'E', 'W'};
+		char mouv = tab[(int)(Math.random() * tab.length)];
+		
+		
+		robot.avancer(mouv);
+		
+		//robot.avancer('S');
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("\\n");
+	}
+}
