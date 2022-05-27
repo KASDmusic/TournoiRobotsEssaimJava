dfe742a7689709675b75318000df6a2a71b03d93
kenzo.lecoindre <kenzo.lecoindre@etu.unicaen.fr>
2022-05-27 20:09:22 +0200

    Redaction du fichier d'execution Bash

--- execute.sh
+++ execute.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+rm -rf bin
+
+cd src
+
+javac -d ../bin/ model/*.java
+javac -d ../bin/ -cp ../bin/ view/*.java
+javac -d ../bin/ Controleur.java
+
+cd ..
+cd bin
+
+java Controleur
+
+cd ..
